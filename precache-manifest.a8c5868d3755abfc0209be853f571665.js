c07165b5da4275e4f50b3d88692b87138e013a71
badwan95 <runner@fv-az20.jc43tw1q12huphtl0dposn5esa.gx.internal.cloudapp.net>
2020-07-04 19:48:47 +0000

    Updates

---
+++ precache-manifest.a8c5868d3755abfc0209be853f571665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90e05919481bc15006e15d6ff3a7fe5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23872c74337228c565aa",
+    "url": "./static/css/main.f4655060.chunk.css"
+  },
+  {
+    "revision": "50c309c81730255bc639",
+    "url": "./static/js/2.50303651.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.50303651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23872c74337228c565aa",
+    "url": "./static/js/main.a81c80b2.chunk.js"
+  },
+  {
+    "revision": "37876e062cbb7b84e726",
+    "url": "./static/js/runtime-main.ed11cb97.js"
+  }
+]);
