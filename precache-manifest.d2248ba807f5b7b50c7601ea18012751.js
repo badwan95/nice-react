37137333f6fa1a06827823b6e537d357cc0fa585
badwan95 <runner@fv-az20.afzsvo1wkdxurorx3wo3rjyloe.gx.internal.cloudapp.net>
2020-07-04 19:52:44 +0000

    Updates

---
+++ precache-manifest.d2248ba807f5b7b50c7601ea18012751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824ad72a2f7225a407d02f4831c13d49",
+    "url": "/nice-react/index.html"
+  },
+  {
+    "revision": "23872c74337228c565aa",
+    "url": "/nice-react/static/css/main.f4655060.chunk.css"
+  },
+  {
+    "revision": "50c309c81730255bc639",
+    "url": "/nice-react/static/js/2.50303651.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nice-react/static/js/2.50303651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23872c74337228c565aa",
+    "url": "/nice-react/static/js/main.a81c80b2.chunk.js"
+  },
+  {
+    "revision": "59b4a30963f9cdaf9566",
+    "url": "/nice-react/static/js/runtime-main.3fcc06e9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a8c5868d3755abfc0209be853f571665.js"
+  "/nice-react/precache-manifest.d2248ba807f5b7b50c7601ea18012751.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nice-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
